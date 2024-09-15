333739f32b7e37f9845c2ec81b0468464e97918c
Jônattas Moraes <jonatas.moraes.09@hotmail.com>
2024-09-14 21:10:06 -0300

    refactor: ajust tailwind config

--- tailwind-spotify/tailwind.config.ts
+++ tailwind-spotify/tailwind.config.ts
@@ -1,14 +1,8 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
 };
-
